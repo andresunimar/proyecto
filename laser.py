51dd4133c33bdc1a1bc004c453aa51cfbee2fb23
andresunimar <aespinel.7257@unimar.edu.ve>
2025-02-26 17:12:50 -0400

    Update laser.py

--- laser.py
+++ laser.py
@@ -10,3 +10,23 @@ servicios_adicionales = 0
 precio_primera = 200
 precio_segunda = 150
 precio_tercera = 100
+
+def ejecutar():
+    while True:
+        print("\nMenú:")
+        print("1. Comprar boleto")
+        print("2. Ver sistema")
+        print("3. Salir")
+        opcion = input("Seleccione una opción: ")
+        if opcion == "1":
+            comprar_boleto()
+        elif opcion == "2":
+            ver_sistema()
+        elif opcion == "3":
+            print("Gracias por usar el sistema de venta de boletos de LASER Airlines.")
+            break
+        else:
+            print("Opción no válida, intente de nuevo.")
+
+ejecutar()
+
